8873e84550aeebe25adc82b5e0836b752ac51ea4
jjssanunga-cpu <jjs.sanunga@yavirac.edu.ec>
2026-06-08 23:45:58 -0500

    calculos

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import {Persona} from './clases/Persona';
+/* import {Persona} from './clases/Persona';
 const persona = new Persona();
 persona.nombre = 'Juan';
 persona.fechaNacimiento = new Date('2000-01-01');
@@ -12,3 +12,19 @@ persona2.fechaNacimiento = new Date('2020-01-01');
 persona2.estatura =180;
 
 persona2.calcularMayorEdad();
+
+
+ */
+////////////////////////////////////////
+
+import {Figura} from './Polimorfismo/Figura';
+import {Rectangulo} from './Polimorfismo/rectangulo';
+import {Pentagono} from './Polimorfismo/Pentagono';
+
+const figura1:Figura= new Rectangulo(5,10);
+console.log( figura1.calcularPerimetro());
+console.log(figura1.imprimirPerimetro());
+
+const figura2:Figura= new Pentagono(5);
+console.log( figura2.calcularPerimetro());
+console.log(figura2.imprimirPerimetro());
